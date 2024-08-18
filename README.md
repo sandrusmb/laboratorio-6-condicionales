8a7417a3249ef195d5d9bd92b75f53f4e11ca163
sandrusmb <sandrusmb@gmail.com>
2024-08-18 20:04:46 +0200

    Add buttons functionalities and styles

--- README.md
+++ README.md
@@ -1,11 +1,80 @@
-# typescript-sandbox
+# Laboratorio 6: Condicionales
 
-Para la parte de cada módulo en la que te planteamos ejercicios con JavaScript puedes usar este sandbox.
+**Cómo compilar el repositorio:**
 
-Pasos:
+- Clona el proyecto
+- Instala npm install
+- Ejecuta npm run dev
+- Abre el navegador en `http://localhost:5173/`
 
-- Clonate el proyecto.
-- Instala las dependencias con `npm install`.
-- Ejecuta el sandbox con `npm run dev`.
-- Abre el navegador en `http://localhost:5173/` (si ese puerto no te funciona, mira en la consola donde has hecho el build, puede que este ocupado y se haya abierto en otro puerto).
-- Puedes empezar a meter tu código en el fichero:
+**Ejercicio:**
+
+1. Mostrar puntuación
+
+Arranca por crear una variable que almacena la puntuación que lleve el usuario:
+
+    Crea una variable para almacenar la puntuación, inicialmente será 0.
+    Crea un div en el HTML en el que podamos mostrar la puntuación.
+    Crea una función que se llame muestraPuntuacion que muestre la puntuación actual en el div.
+    Invoca a la función en cuanto este disponible el DOM.
+
+    Más adelante invocaremos muestraPuntuación cada vez que el usuario pida carta nueva.
+
+2. Pedir carta
+
+Implementa la funcionalidad de pedir carta, ¿En qué consiste?
+
+    Hay que generar una función que nos devuelva una carta aleatoria, la podemos llamar dameCarta.
+    Para ello exponemos un botón en el HTML que al pulsarlo llame a la función dameCarta.
+    Para probar este caso, de momento muestra la carta elegida por consola.
+
+Pistas:
+
+    Las cartas tienen los siguientes valores: 1,2,3,4,5,6,7,10,11,12
+    Hasta ahora math.Random lo hemos usado para obtener números aleatores de un rango continuo (por ejemplo de 0 a 100), en este caso nos queremos saltar el 8 y el 9, SPOILER ALERT (piensa en una solución antes de leer la siguiente pista :))... ¿Cómo podemos hacerlo? Puedes plantear generar un número aleatorio entre 1 y 10, si el número es mayor que 7, le sumas 2 y ya tienes los valores que necesitabas.
+
+3. Mostrar carta
+
+   Crea una función que se llame muestraCarta que muestre la carta que le pasemos por parámetro, la firma podría ser tal que así:
+
+const mostrarCarta = (carta: number) : void;
+
+Pistas
+
+    Añade un img en el HTML en el que podamos mostrar la carta.
+
+    Ese img va a tener un src que va a ser la url de la imagen de la carta, de momento, utiliza la imagen de carta boca abajo: https://github.com/Lemoncode/fotos-ejemplos/blob/main/cartas/back.jpg
+
+    Crea una función mostrar carta, para mapear valor a imagen de carta puedes utilizar un switch para hacer la conversión, recuerda que más arriba tienes los enlaces a las imágenes de las cartas.
+
+    Cuando el usuario pulse en el bóton Pide Carta llama a pideCarta y con el resultado llama a mostrarCarta.
+
+4. Game over
+
+Si el usuario se pasa de 7,5 puntos, el juego termina y se muestra un mensaje de Game Over, además el usuario no puede seguir pidiendo cartas.
+
+5. Me planto
+
+Añadir un botón para que el usuario pueda plantarse, si el usuario se planta, el juego termina, el usuario no puede pedir más cartas y:
+
+    Si su puntuación es menor que 4, mostrar un mensaje que diga "Has sido muy conservador".
+
+    Si la puntuación ha sido 5, mostrar un mensaje que diga "Te ha entrado el canguelo eh?".
+
+    Si la puntuación ha sido 6 o 7, mostrar un mensaje que diga... "Casi casi...".
+
+    Si la puntuación es 7.5, mostrar un mensaje que diga "¡ Lo has clavado! ¡Enhorabuena!"
+
+6. Nueva partida
+
+Una vez que el usuario ha terminado la partida (sea porque se ha plantado o porque ha perdido), se le muestra un botón para que pueda empezar una nueva partida.
+
+7. Estila la aplicación
+
+Utilizando CSS, estila la aplicación (margenes, espacios, colores, etc...) para que tenga el mejor aspecto posible.
+
+Apartado opcional
+
+Saber lo que habría pasado
+
+Una vez que el usuario ya se ha plantado, se le muestra un botón para que pueda saber lo que habría pasado si hubiera seguido pidiendo cartas.
--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,196 @@
 import "./style.css";
 
-let cards = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+let score: number = 0;
+const cardButton = document.querySelector(".card-button");
+const stopButton = document.querySelector(".stop-button");
+const resetButton = document.querySelector(".reset-button");
+const container = document.querySelector(".card-container");
+const containerSecondary = document.querySelector(".container-secondary");
 
-const buttonStart = document.querySelector(".button-start");
-const card = document.querySelector(".card");
-
-function mixCards() {
-  cards.sort(() => Math.random() - 0.5);
-  console.log(cards);
-  paintCards();
-}
-
-function paintCards() {
-  console.log(cards);
-  let container = document.querySelector(".container");
-  if (container !== null) {
-    container.innerHTML = "";
-    cards.forEach((card) => {
-      let img = document.createElement("img");
-      img.textContent = card;
-      img.src =
-        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
-      img.classList.add("card");
-      img.classList.add("card" + "-" + card);
-      if (container !== null) {
-        container.appendChild(img);
-      }
-    });
+function showScore() {
+  let scoreContainer = document.querySelector(".score");
+  if (scoreContainer !== null) {
+    scoreContainer.textContent = "Puntuación:" + " " + score.toString();
   }
 }
 
-function turnCard() {
-  console.log("hola");
+function giveMeCard(): number {
+  let cardNumber = Math.floor(Math.random() * 10) + 1;
+  if (cardNumber > 7) {
+    cardNumber += 2;
+  }
+  console.log(cardNumber);
+  return cardNumber;
 }
 
-if (card !== null) {
-  card.addEventListener("click", turnCard);
+function showCard(card: number): void {
+  let cardImageUrl =
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
+
+  if (container !== null) {
+    switch (card) {
+      case 1:
+        cardImageUrl =
+          "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg";
+        break;
+      case 2:
+        cardImageUrl =
+          "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg";
+        break;
+      case 3:
+        cardImageUrl =
+          "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg";
+        break;
+      case 4:
+        cardImageUrl =
+          "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg";
+        break;
+      case 5:
+        cardImageUrl =
+          "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
+        break;
+      case 6:
+        cardImageUrl =
+          "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
+        break;
+      case 7:
+        cardImageUrl =
+          "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
+        break;
+      case 10:
+        cardImageUrl =
+          "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
+        break;
+      case 11:
+        cardImageUrl =
+          "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
+        break;
+      case 12:
+        cardImageUrl =
+          "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
+        break;
+      default:
+        cardImageUrl =
+          "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
+        break;
+    }
+
+    container.innerHTML += `<img class="card" src="${cardImageUrl}" alt="Carta"/>`;
+  }
 }
 
-if (buttonStart !== null) {
-  buttonStart.addEventListener("click", mixCards);
+function calculateScore(card: number): void {
+  let cardValue: number;
+
+  switch (card) {
+    case 1:
+      cardValue = 1;
+      break;
+    case 2:
+      cardValue = 2;
+      break;
+    case 3:
+      cardValue = 3;
+      break;
+    case 4:
+      cardValue = 4;
+      break;
+    case 5:
+      cardValue = 5;
+      break;
+    case 6:
+      cardValue = 6;
+      break;
+    case 7:
+      cardValue = 7;
+      break;
+    case 10:
+      cardValue = 0.5;
+      break;
+    case 11:
+      cardValue = 0.5;
+      break;
+    case 12:
+      cardValue = 0.5;
+      break;
+    default:
+      cardValue = 0;
+      break;
+  }
+  score += cardValue;
+  showScore();
 }
+
+function showMessage() {
+  if (score > 7.5) {
+    alert("Game over");
+  } else if (score < 4) {
+    alert("Has sido muy conservador");
+  } else if (score === 5) {
+    alert("Te ha entrado el canguelo eh?");
+  } else if (score === 6 || score == 7) {
+    alert("Casi casi...");
+  } else if (score === 7.5) {
+    alert("¡ Lo has clavado! ¡Enhorabuena!");
+  }
+}
+
+function disableCardButton() {
+  if (cardButton !== null) {
+    cardButton.setAttribute("disabled", "true");
+  }
+}
+
+function enableCardButton() {
+  if (cardButton !== null) {
+    cardButton.removeAttribute("disabled");
+  }
+}
+
+function whatIsNext() {
+  if (containerSecondary !== null) {
+    containerSecondary.innerHTML += `<button class="button next-button">¿Qué hubiera pasado?</button>`;
+
+    const nextButton = document.querySelector(".next-button");
+    if (nextButton !== null) {
+      nextButton.addEventListener("click", () => {
+        const card = giveMeCard();
+        showCard(card);
+      });
+    }
+  }
+}
+
+function reset() {
+  score = 0;
+  showScore();
+
+  if (container !== null) {
+    container.innerHTML = "";
+  }
+
+  enableCardButton();
+}
+
+if (cardButton !== null) {
+  cardButton.addEventListener("click", () => {
+    const card = giveMeCard();
+    showCard(card);
+    calculateScore(card);
+  });
+}
+
+if (stopButton !== null) {
+  stopButton.addEventListener("click", () => {
+    showMessage();
+    disableCardButton();
+    whatIsNext();
+  });
+}
+
+if (resetButton !== null) {
+  resetButton.addEventListener("click", reset);
+}
+
+enableCardButton();
+showScore();
